3f0f0bd4acfabdbd9cd2c1f0a4d12d38d27c87f3
AnshSachinkumarArora <ansh47arora47@gmail.com>
2026-09-12 22:44:54 +0530

    removed dead code from gpt

--- gpt/gpt.py
+++ gpt/gpt.py
@@ -9,12 +9,7 @@ import tiktoken
 from flash_attention.triton.flash_attention2 import custom_flash_attention_2
 import time
 
-#load dataset
-'''with open('./dataset/input.txt', 'r', encoding='utf-8') as file:
-    text = file.read()
-chars = sorted(list(set(text)))'''
-
-#hyperparams
+## hyperparams
 if torch.cuda.is_available():
     device = 'cuda'
 elif mps.is_available():
@@ -22,12 +17,12 @@ elif mps.is_available():
 else:
     device = 'cpu'
 vocab_size = 100256
-embed_dim = 512 #C
-num_heads = 4 #nh
-block_size = 256 #T
-batch_size = 64 #B
+embed_dim = 512 ## C
+num_heads = 4 ## nh
+block_size = 256 ## T
+batch_size = 64 ## B
 mini_batch_size = 4
-head_dim = embed_dim//num_heads #hs
+head_dim = embed_dim//num_heads ## hs
 num_iters = 1000
 num_blocks = 1
 learning_rate = 6e-4
@@ -35,20 +30,14 @@ dataset_path = 'dataset/dataset.bin'
 encoder = tiktoken.get_encoding('cl100k_base')
 max_seq_len = 1024
 
-#basic encoding and decoding
-'''stoi = { ch: i for i, ch in enumerate(chars) }
-itos = { i: ch for i, ch in enumerate(chars) }
-encode = lambda s: [stoi[c] for c in s]
-decode = lambda l: ''.join([itos[i] for i in l])'''
-
-#dataset creation
+## dataset creation
 dataset = np.memmap(dataset_path, dtype=np.uint32, mode='r')
-#train/val split
+## train/val split
 split_size = int(0.9*len(dataset))
 train = dataset[:split_size]
 val = dataset[split_size:]
 
-#data loader
+## data loader
 def Generate_Batch(split):
     data = train if split == 'train' else val
     ix = torch.randint(len(data) - block_size, (mini_batch_size,))
@@ -87,30 +76,30 @@ class Embedding(nn.Module):
 class RoPE(nn.Module):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        #setting up positions and frequencies
+        ## setting up positions and frequencies
         pos = torch.arange(max_seq_len, device=device)
         freq = 1/(10000**(torch.arange(0, head_dim, 2).float()/head_dim)).to(device=device)
         pos_freq = torch.outer(pos, freq)
         sin_cache = torch.sin(pos_freq)
         cos_cache = torch.cos(pos_freq)
-        #setting up sin and cos caches
+        ## setting up sin and cos caches
         self.register_buffer('sin_cache', torch.cat((sin_cache, sin_cache), dim=1))
         self.register_buffer('cos_cache', torch.cat((cos_cache, cos_cache), dim=1))
 
     def forward(self, q, k, start_pos=0):
         B, nh, T, hs = q.shape
-        #splitting and negating
+        ## splitting and negating
         q_split = self.neg_split(q)
         k_split = self.neg_split(k)
-        #getting correct cache values
+        ## getting correct cache values
         sin = self.sin_cache[start_pos : start_pos + T, :]
         cos = self.cos_cache[start_pos : start_pos + T, :]
-        #performing RoPE
+        ## performing RoPE
         pos_embedded_q = (q * cos) + (q_split * sin)
         pos_embedded_k = (k * cos) + (k_split * sin)
         return pos_embedded_q, pos_embedded_k
     
-    #helper function to negate second half of tensor dim -1 and flip it
+    ## helper function to negate second half of tensor dim -1 and flip it
     def neg_split(self, x):
         x1, x2 = torch.chunk(x, chunks=2, dim=-1)
         output = torch.cat((-x2, x1), dim=-1)
@@ -130,40 +119,40 @@ class CausalSelfAttention(nn.Module):
 
     def forward(self, tokens, use_cache=False, absolute_pos=0, use_flash_attention=False):
         B, T, C = tokens.shape
-        wei = self.weights(tokens) #(B, T, 3C)
+        wei = self.weights(tokens) ## (B, T, 3C)
 
-        #split into QKV matrices of shape (B, T, C)
+        ## split into QKV matrices of shape (B, T, C)
         q = wei[:, :, :C]
         k = wei[:, :, C:C*2]
         v = wei[:, :, C*2:]
 
-        #reshape for correct dimensions per head
+        ## reshape for correct dimensions per head
         q = q.reshape(B, T, num_heads, head_dim).transpose(1, 2)
         k = k.reshape(B, T, num_heads, head_dim).transpose(1, 2)
         v = v.reshape(B, T, num_heads, head_dim).transpose(1, 2)
 
-        #TRAINING 
+        ## TRAINING 
         if use_cache is False:
             q, k = self.rope(q, k)
             sm_scale = k.shape[-1]**(-0.5)
             if use_flash_attention is False:
-                #calculate scaled QK^t, shape (B, T, T)
+                ## calculate scaled QK^t, shape (B, T, T)
                 qkt = q @ k.transpose(-2, -1)
                 qkt_scaled = qkt * sm_scale
                 qkt_masked = qkt_scaled.masked_fill(self.causal_mask[:T, :T] == 0, float('-inf'))
                 qkt_softmax = F.softmax(qkt_masked, dim=-1)
-                #calculating scaled dot product attention, shape (B, T, C)
+                ## calculating scaled dot product attention, shape (B, T, C)
                 scaled_dot_attn = qkt_softmax @ v
             else:
                 scaled_dot_attn, _ = custom_flash_attention_2.apply(q, k, v, True, sm_scale)
-        #INFERENCE 
+        ## INFERENCE 
         else:
             q, k = self.rope(q, k, start_pos=absolute_pos)
             cache_pos = absolute_pos % block_size
-            #append to cache
+            ## append to cache
             self.k_cache[:B, :, cache_pos:cache_pos+1, :] = k
             self.v_cache[:B, :, cache_pos:cache_pos+1, :] = v
-            #get correct number of kv values
+            ## get correct number of kv values
             if absolute_pos < block_size:
                 k_hist = self.k_cache[:B, :, :absolute_pos+1, :]
                 v_hist = self.v_cache[:B, :, :absolute_pos+1, :]
@@ -171,16 +160,16 @@ class CausalSelfAttention(nn.Module):
                 k_hist = self.k_cache[:B, :, :, :]
                 v_hist = self.v_cache[:B, :, :, :]
 
-            #perform attention calculation
+            ## perform attention calculation
             qkt = q @ k_hist.transpose(-2, -1)
             qkt_scaled = qkt * k.shape[-1]**(-0.5)
             qkt_softmax = F.softmax(qkt_scaled, dim=-1)
             scaled_dot_attn = qkt_softmax @ v_hist
 
-        #reshape attention tensor
+        ## reshape attention tensor
         scaled_dot_attn = scaled_dot_attn.transpose(1, 2).reshape(B, T, C)
 
-        #calculating final output 
+        ## calculating final output 
         output = self.output_weights(scaled_dot_attn)
 
         return output
@@ -269,13 +258,13 @@ class GPT(nn.Module):
 model = GPT(num_blocks)
 model = model.to(device)
 
-#training loop
+## training loop
 optimizer = AdamW(model.parameters(), lr=learning_rate)
-#splitting into mini_batches due to gpu memory constraints
 
-#time training loop
+## time training loop
 start_time = time.perf_counter()
 
+## splitting into mini_batches due to gpu memory constraints
 grad_steps = int(batch_size/mini_batch_size)
 for step in range(num_iters):
     optimizer.zero_grad(set_to_none=True)
